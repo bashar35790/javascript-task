a30dd9a1ae81437d9bbc04cace3914da71de9907
Bashar <bashar35790@gmail.com>
2026-01-25 12:21:18 +0600

    practice cancat mathod

---
+++ concat.js
@@ -0,0 +1,16 @@
+const fish = ["magur", "elish", "rupcada", "kholsi pish"];
+
+
+const animal = ["bag", "singho", "shiyal", "khorgos"];
+
+
+
+const combained = fish.concat(animal);
+
+console.log(fish);
+console.log(animal);
+console.log(combained);
+
+
+
+
---
+++ isArray.js
@@ -0,0 +1,17 @@
+const book = ["bangla", "engish", "science"];
+const fruits = [];
+const number = [2,4,5 ,6,7];
+
+const Name = 'Abul Bashar';
+const roll = 3;
+const student = true;
+
+
+console.log(Array.isArray(book));
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(number));
+console.log(Array.isArray(Name));
+console.log(Array.isArray(roll));
+console.log(Array.isArray(student));
+
+
