3ece39a17c769b71077a7421bcbebebbf61afa38
Bashar <bashar35790@gmail.com>
2026-01-25 12:06:57 +0600

    practice array include method

---
+++ include.js
@@ -0,0 +1,13 @@
+const book = ["Himu", "Oporicila", "Moner Pakhi"];
+
+const b = book.includes("Oporicila");
+
+const k = book.includes("kds");
+
+const p = book.includes("himu");
+
+
+console.log(p, k, b);
+
+
+
